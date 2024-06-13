a84d6300f22190f95aeaa92ea1be38811b83e09e
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2024-06-13 12:11:01 -0300

    v.24.06.13.02 - Melhorado footer

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
-    <div class="footer bg-indigo-800 mt-8 p-5" style="border-radius: 0.4rem;">
-        <div class="flex justify-content-between text-gray-100 flex-wrap gap-4">
+    <div class="footer bg-indigo-800 mt-3 p-5 pt-2 pb-2" style="border-radius: 1rem 1rem 0 0;">
+        <div class="flex justify-content-between text-gray-100 flex-wrap gap-4" style="font-size: 14px;">
             <span>
                 <span>Gustavo Rodrigues - Web Developer</span>
-                <span> - v. 24.06.13.01</span>
+                <span> - v. 24.06.13.02</span>
             </span>
             <span>
                 <span>Marcos Júnior © </span>
